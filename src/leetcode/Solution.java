53aabd85a35882b9299af3a5e087ca2625659417
Your Name <akmugli7@gmail.com>
2026-05-29 12:07:11 +0530

    Added leetcode folder

---
+++ src/leetcode/Solution.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static int[] twosum(int [] arr ,int target) {
+	int [] res =new int[2];
+	for(int i=0;i<arr.length-1;i++) {
+		for (int j=i+1;j<arr.length;j++) {
+			if(arr[i]+arr[j]==target) {
+				res[0]=i;
+				res[1]=j;
+			}
+		}
+	}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {3,2,4};
+		int target=6;
+		int resc[]=twosum(arr,target);
+		System.out.println(Arrays.toString(resc));
+
+
+	}
+
+}
