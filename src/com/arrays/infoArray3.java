1af6eafeb74d7ddc7c0083930061c05e3996cc06
Ankita <akmugli7@gmail.com>
2026-06-07 22:53:37 +0530

    Add functionality to find minimum in new array

--- src/com/arrays/infoArray3.java
+++ src/com/arrays/infoArray3.java
@@ -47,6 +47,16 @@ public class infoArray3 {
 		}
 		System.out.println(min3);//-20
 
+			int[] arr3= {16,10,47,15,4,8};
+		int min4=Integer.MAX_VALUE;
+		for(int ele:arr) {
+			if(ele<min3) {
+				min4=ele;
+			}
+		}
+		System.out.println(min4);//
+
+
 	}
 
 }
