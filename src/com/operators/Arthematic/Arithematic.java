f1f4eb94f76b650ff8320f18c48ece94efccf607
Ankita <akmugli7@gmail.com>
2026-06-21 11:02:37 +0530

    Update Arithematic.java

--- src/com/operators/Arthematic/Arithematic.java
+++ src/com/operators/Arthematic/Arithematic.java
@@ -3,12 +3,10 @@ package com.operators.Arthematic;
 public class Arithematic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	System.out.println(101-201);
 		System.out.println(125+520);
 		System.out.println(201-100);  
 		System.out.println(101+101);  
 		System.out.println(201*100);
-
 	}
 
 }
